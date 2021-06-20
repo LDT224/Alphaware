a3ace7b3efa90767fda3dab6179e91eb0655b550
LDT224 <ledoantai2204@gmail.com>
2021-06-20 13:44:50 +0700

    Thay doi thong tin ket noi

---
+++ db/dbconn.php
@@ -0,0 +1,14 @@
+<?php
+	$conn_string = "host=ec2-34-193-112-164.compute-1.amazonaws.com"
+        . " port=5432 "
+        . " dbname=datn1rj22fsn1f"
+        . " user=rwatmezyromljv"
+        . " password=6a894227dfa4248780670eb3f3a78baf4f1c45d6a235f22f1f80a94e08177c1e";
+$db = pg_connect($conn_string);
+//connect to a database named "test" on the host "sheep" with a username and password
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
+?>
